41f94c20b28009442025f6a0873a102ad5e6de19
Jung Hoan Kim <kjunghoan@gmail.com>
2024-07-04 23:14:59 +0000

    Changing comments

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,13 +1,13 @@
 // import bcrypt from 'bcrypt';
 
-// /**
-//  *
-//  * @param {input} - The password from the input form
-//  * @param {storedPassword} - The password from the database
-//  * @description Tests the password from the input form to the password stored in the database
-//  *
-//  * @returns Boolean
-//  */
+/**
+ *
+ * @param {input} - The password from the input form
+ * @param {storedPassword} - The password from the database
+ * @description Tests the password from the input form to the password stored in the database
+ *
+ * @returns Boolean
+ */
 // export const passwordsMatch = async (input: string, storedPassword: string) => {
 //   return await bcrypt.compare(input, storedPassword);
 // };
