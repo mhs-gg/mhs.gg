75c3600adcb5656b847446d2889a331f60b01b10
Jung Hoan Kim <kjunghoan@gmail.com>
2024-07-06 10:35:14 +0000

    enable lazy loading on auth

--- src/auth.ts
+++ src/auth.ts
@@ -11,46 +11,56 @@ import { findUserByEmail } from "./services/userServices";
 interface NextAuthUser {
   id: string;
   name: string;
+  password: string;
   email: string;
   image?: string;
 }
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: MongoDBAdapter(user_repository),
-  callbacks: {
-    authorized: async ({auth}) => {
-      return !!auth;
-    },
-  },
-  providers: [
-    Google,
-    Credentials({
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      authorize: async (credentials) => {
-        const parsedCredentials = signInSchema.safeParse(credentials);
+export const { handlers, signIn, signOut, auth } = NextAuth( (req?) => {
+  
+  if (req) {
+    console.log('This is the request')
+    console.log (req)
+  }
 
-        if (parsedCredentials.success) {
-          const {email, password} = parsedCredentials.data;
-          const user = await findUserByEmail(email);
-          if (!user) return null;
-          if (await bcrypt.compare(password, user.password)) {
-            const nextAuthUser: NextAuthUser = {
-              id: user._id.toString(),
-              name: user.name,
-              email: user.email,
-              image: user.image,
-            };
-            return nextAuthUser;
-          }
-          console.log('Invalid credentials');
-        }
-        return null;
+  return {
+    adapter: MongoDBAdapter(user_repository),
+    callbacks: {
+      authorized: async ({auth}) => {
+        return !!auth;
       },
-    }),
-  ],
+    },
+    providers: [
+      Google,
+      Credentials({
+        credentials: {
+          email: { label: "Email", type: "text" },
+          password: { label: "Password", type: "password" },
+        },
+        // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+        // e.g. domain, username, password, 2FA token, etc.
+        authorize: async (credentials) => {
+          const parsedCredentials = signInSchema.safeParse(credentials);
+
+          if (parsedCredentials.success) {
+            const {email, password} = parsedCredentials.data;
+            const user = await findUserByEmail(email);
+            if (!user) return null;
+            if (await bcrypt.compare(password, user.password)) {
+              const nextAuthUser: NextAuthUser = {
+                id: user._id.toString(),
+                name: user.name,
+                password: user.password,
+                email: user.email,
+                image: user.image,
+              };
+              return nextAuthUser;
+            }
+            console.log('Invalid credentials');
+          }
+          return null;
+        },
+      }),
+    ],
+  }
 })
